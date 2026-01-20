d6c12eb97a616d237a7b0c85c9f64a633317fb0b
Rachel Pirnie <rachel.pirnie@gmail.com>
2026-01-19 19:49:53 -0800

    Step 2.3 Complete

--- w2/app.js
+++ w2/app.js
@@ -17,19 +17,19 @@ const itemPrice = 15;
 const handleButtonClick = function() {
     // totalCost = totalCost + 1;
     // Increase totalCost by 1 each time the button is clicked
-    totalCost += 1;
+    totalCost += itemPrice;
     // Template strings (literal) to easily combine our variables and text into one message
-    let total = `Hello! You have clicked the button ${totalCost} time(s).`;
+    let total = `Current Total: $${totalCost}`;
 
     // This is basic decision-making in JavaScript!
     // Use a simple 'if' statement to make our page react differently based on totalCost.
-    if(totalCost >= 5) {
+    if(totalCost >= 60) {
         // We can even change the style of an HTML element directly with JavaScript!
-        total += ' WOW, you are a super clicker!';
+        total += ' (Over Budget!)';
         // Change text color
-        totalDisplayElement.style.color = 'blue';
+        totalDisplayElement.style.color = 'red';
     } else {
-        totalDisplayElement.style.color = '#009933';
+        totalDisplayElement.style.color = 'black';
     };
     
     // Update the text content of our paragraph element on the page.
